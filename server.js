6c6f141ee586d6a664b4bdd66fa1c1a9567494e3
Princesharma03 <prince.sharma@innovaccer.com>
2022-05-01 17:43:43 +0530

    new commit

---
+++ server.js
@@ -0,0 +1,27 @@
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+
+io.on('connection', (socket)=> {
+      console.log('User Online');
+      socket.on('join-room',room=>{
+            console.log(`room joined ${room}`)
+            socket.join(room);
+      })
+      socket.on('canvas-data', (data,room)=> {
+            console.log(`${room}`)
+            // socket.join(room);
+            if(room != ""){
+                  console.log("room executed")
+                  io.to(room).emit('canvas-data', data);
+            }else{
+                  console.log("room don't executed")
+                  socket.broadcast.emit('canvas-data', data);
+            }   
+      })
+})
+
+var server_port = process.env.YOUR_PORT || process.env.PORT || 5000;
+http.listen(server_port, () => {
+    console.log("Started on : "+ server_port);
+})
